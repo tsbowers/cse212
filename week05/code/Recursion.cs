34f165281b37a482ed35bf8118645d1d52b2d52a
tsbowers <trevorbowers3@gmail.com>
2026-06-08 20:10:43 -0500

    week 5

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,9 +15,13 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
-    }
+if (n <= 0)
+{
+    return 0;
+}
 
+return n * n + SumSquaresRecursive(n - 1);
+    }
     /// <summary>
     /// #############
     /// # Problem 2 #
@@ -40,6 +44,17 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+{
+    results.Add(word);
+    return;
+}
+
+for (var i = 0; i < letters.Length; i++)
+{
+    var lettersLeft = letters.Remove(i, 1);
+    PermutationsChoose(results, lettersLeft, size, word + letters[i]);
+}
     }
 
     /// <summary>
@@ -97,12 +112,23 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+if (remember == null)
+{
+    remember = new Dictionary<int, decimal>();
+}
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
+if (remember.ContainsKey(s))
+{
+    return remember[s];
+}
+
+// Solve using recursion
+decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+
+remember[s] = ways;
+
+return ways;
     }
-
     /// <summary>
     /// #############
     /// # Problem 4 #
@@ -119,6 +145,16 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        var index = pattern.IndexOf('*');
+
+if (index == -1)
+{
+    results.Add(pattern);
+    return;
+}
+
+WildcardBinary(pattern[..index] + "0" + pattern[(index + 1)..], results);
+WildcardBinary(pattern[..index] + "1" + pattern[(index + 1)..], results);
     }
 
     /// <summary>
